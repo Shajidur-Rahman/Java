a8917ffcaa10bc88f5b104e0567b1ee22d8ebf0a
Shajidur-Rahman <shajidurrahmansaad@10gmail.com>
2021-07-06 04:36:16 -0700

    testing

--- src/com/company/java_02_ch_12_vi_02_pr.java
+++ src/com/company/java_02_ch_12_vi_02_pr.java
@@ -3,5 +3,6 @@ package com.company;
 public class java_02_ch_12_vi_02_pr {
     public static void main(String[] args) {
         System.out.println(3434);
+        System.out.println("Os changed");
     }
 }
