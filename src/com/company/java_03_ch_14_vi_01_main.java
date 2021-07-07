6673d674c9979a9b71b1a3c8a1203d6778b2e587
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-07-07 18:49:33 +0600

    tutorial continue 16:00 14 video

---
+++ src/com/company/java_03_ch_14_vi_01_main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Locale;
+
+public class java_03_ch_14_vi_01_main {
+    public static void main(String[] args) {
+//        String name  = "Shajidur";
+//        int value = name.length();
+//        System.out.println(value);
+//        String name2 = "Shajidur";
+//        System.out.println(name2.length());
+//        String name3 = "Shajidur";
+//       System.out.println(name3.toLowerCase());
+//        String name4 = "                        My name is shajdiu rahman";
+//        System.out.println(name4.trim());
+        String name5 = "Shajidur rahman";
+        System.out.println(name5.replace("S", "R"));
+        // 16:36
+    }
+}
