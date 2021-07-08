07346244207388c839e9eaa27a973fc2d68d3aa0
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-07-08 09:32:34 -0700

    continue 26:00

--- src/com/company/java_03_ch_14_vi_01_main.java
+++ src/com/company/java_03_ch_14_vi_01_main.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 
 public class java_03_ch_14_vi_01_main {
     public static void main(String[] args) {
-//        String name  = "Shajidur";
+        String name  = "Shajidur";
 //        int value = name.length();
 //        System.out.println(value);
 //        String name2 = "Shajidur";
@@ -13,8 +13,13 @@ public class java_03_ch_14_vi_01_main {
 //       System.out.println(name3.toLowerCase());
 //        String name4 = "                        My name is shajdiu rahman";
 //        System.out.println(name4.trim());
-        String name5 = "Shajidur rahman";
-        System.out.println(name5.replace("S", "R"));
-        // 19:36
+//        String name5 = "Shajidur rahman";
+//        System.out.println(name5.replace("S", "R"));
+//        System.out.println(name.startsWith("My"));
+//        System.out.println(name.startsWith("Sh"));
+//        System.out.println(name.endsWith("Shajidur"));
+//        System.out.println(name.charAt(2));
+//        System.out.println(name.indexOf("i"));
+        //26:00
     }
 }
