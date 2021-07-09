57dc4e9f491c68998ea8174f7b15e06e1823cc3d
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-07-09 01:36:20 -0700

    chapter 03 added

---
+++ src/com/company/java_03_ch_14_vi_02_main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class java_03_ch_14_vi_02_main {
+    public static void main(String[] args) {
+        String name;
+        name = new String("Shajidur");
+        System.out.println(name);
+    }
+}
