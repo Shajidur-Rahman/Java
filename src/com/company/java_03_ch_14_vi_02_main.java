a6366d82823e9ed18897921698c531b9efb3b88c
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-07-09 01:48:44 -0700

    chapter 03 finished by me

--- src/com/company/java_03_ch_14_vi_02_main.java
+++ src/com/company/java_03_ch_14_vi_02_main.java
@@ -3,7 +3,17 @@ package com.company;
 public class java_03_ch_14_vi_02_main {
     public static void main(String[] args) {
         String name;
+//        name = new String("Shajid\nur");
         name = new String("Shajidur");
-        System.out.println(name);
+//        System.out.println(name);
+//        System.out.printf("My name is %s", name);
+//        System.out.println(name.length());
+        //System.out.println(name.substring(1,5));
+//        System.out.println(name.replace("Sh", "sh"));
+//        System.out.println(name.indexOf("S", 3));
+//        System.out.println(name.equals("Shajidur"));
+//        System.out.println(name.equalsIgnoreCase("Shajidur"));
+//        System.out.println(name);
+
     }
 }
