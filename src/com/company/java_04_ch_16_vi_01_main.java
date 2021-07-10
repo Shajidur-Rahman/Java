df978293ea90b8415b125ea273a681d58f331eee
Shajidur Rahman <shajidurrahmansaad10@gmail.com>
2021-07-10 22:21:06 +0600

    tutorial 8:29

--- src/com/company/java_04_ch_16_vi_01_main.java
+++ src/com/company/java_04_ch_16_vi_01_main.java
@@ -2,6 +2,13 @@ package com.company;
 
 public class java_04_ch_16_vi_01_main {
     public static void main(String[] args) {
-
+        int age = 13;
+        if(age>18){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No ✋");
+        }
+        // 8:29
     }
 }
