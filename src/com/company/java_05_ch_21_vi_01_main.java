f4a950da7b800bc706b3f23433630b5c053c6f88
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-07-18 22:55:16 +0600

    yes

--- src/com/company/java_05_ch_21_vi_01_main.java
+++ src/com/company/java_05_ch_21_vi_01_main.java
@@ -1,4 +1,14 @@
 package com.company;
+import java.util.Scanner;
 
 public class java_05_ch_21_vi_01_main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int tem = 1;
+        while (tem<num){
+            System.out.println(tem);
+            tem++;
+        }
+    }
 }
