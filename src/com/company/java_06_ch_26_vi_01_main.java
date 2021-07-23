a7b70ae24b310644d247c942f9391192d5ec674b
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-07-23 22:28:56 +0600

    tutorial 26 started

---
+++ src/com/company/java_06_ch_26_vi_01_main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class java_06_ch_26_vi_01_main {
+    public static void main(String[] args) {
+//        int [] marks = new int[5];
+//        marks[0] = 100;
+//        marks[1] = 90;
+//        marks[2] = 80;
+//        marks[3] = 70;
+//        marks[4] = 60;
+//        System.out.println(marks[1]);
+        int [] marks = {1,2,3,4,5,6,7,8,9};
+        System.out.println(marks[2]);
+    }
+}
