b125ffad5c0ce34d01c2cb21f7d7fd658d84ea62
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-07-23 18:32:59 +0600

    chapter 5 problem finished

---
+++ src/com/company/java_05_ch_25_vi_pr.java
@@ -0,0 +1,52 @@
+package com.company;
+import java.util.Scanner;
+
+public class java_05_ch_25_vi_pr {
+    public static void main(String[] args) {
+        // problem 1
+        //System.out.println("*" + 4);
+        //problem 2
+          Scanner input = new Scanner(System.in);
+//        int num = input.nextInt();
+//        int time = 0;
+//        int total = 0;
+//        while (time< num) {
+//            if (time%2 == 0){
+//                total = total + time;
+//
+//            }
+//           time++;
+//        }
+//        System.out.println(total);
+        // problem 3
+//        int given_num = input.nextInt();
+//        for (int x=1; x<=10; x++){
+//            System.out.printf("%d * %d = %d\n", given_num, x, (given_num*x));
+        //problem 4
+//         int given_num = input.nextInt();
+//         for (int x=10; x>=1; x--){
+//         System.out.printf("%d * %d = %d\n", given_num, x, (given_num*x));
+
+        //problem 5
+//        int fact = 1;
+//        int num = input.nextInt();
+//        for (int x=1; x<=num; x++){
+//            fact = x*fact;
+//        }
+//        System.out.printf("Factorial = %d\n", fact);
+        //problem 6
+        // problem 7
+        // problem 8
+        // problem 9
+//        int given_num = input.nextInt();
+//        int mul = 1;
+//        for (int x=1; x<=10; x++) {
+//            mul = mul + (given_num * x);
+//        }
+//        System.out.println(mul);
+        //problem 10
+        //problem 11
+        //problem finished
+    }
+
+}
