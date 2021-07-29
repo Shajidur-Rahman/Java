d9221bdbead8c95df7a8b0185c427fc2f429b4e4
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-07-29 18:23:03 +0600

    started

---
+++ src/com/company/java_06_ch_27_vi_01_main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class java_06_ch_27_vi_01_main {
+    public static void main(String[] args) {
+        int [] marks = {2,3,4,5,6,77,8,8};
+//        System.out.println(marks[2]);
+//        System.out.println(marks.length);
+//        int length = (marks.length) -1;
+//        for (int a = 0; a <= length;a++){
+//            System.out.println(marks[a]);
+//        }
+    }
+}
