759fec2416dfe592bdd8ca2fe103403bc3bfbb89
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-08-06 17:47:26 +0600

    video 34 finished

--- src/com/company/java_07_ch_34_vi_01_main.java
+++ src/com/company/java_07_ch_34_vi_01_main.java
@@ -1,14 +1,24 @@
 package com.company;
+import java.util.Scanner;
 
 public class java_07_ch_34_vi_01_main {
     static void factorial(int a){
         int result = 1;
+        if (a==0 || a==1){
+            System.out.println("The factorial of "+ a + " is " + 1);
+        }
+        else {
         for (int x = 1; x <= a; x++){
             result = x * result;
         }
-        System.out.println(result);
+        System.out.println("The factorial of "+ a + " is " + result);
     }
+    }
+
     public static void main(String[] args) {
-        factorial(3);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Give me the number : ");
+        int num = input.nextInt();
+        factorial(num);
     }
 }
