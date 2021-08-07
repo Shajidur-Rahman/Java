de92d26c27e30d195f9ce04117b9007808857e0a
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-08-07 22:45:30 +0600

    video 38 continue

--- src/com/company/java_08_ch_36_vi_01_main.java
+++ src/com/company/java_08_ch_36_vi_01_main.java
@@ -1,7 +1,14 @@
 package com.company;
 
+class name{
+    String name = "Shajidur Rahman";
+}
+
 public class java_08_ch_36_vi_01_main {
     public static void main(String[] args) {
         System.out.println("This is class");
+        name my_name = new name();
+        my_name.name = "My name is shajidur rahman";
+        System.out.println(my_name.name);
     }
 }
