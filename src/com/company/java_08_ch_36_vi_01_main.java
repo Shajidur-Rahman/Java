3f802e6fe6b086657a930b81af5d5fa613ceb599
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-08-08 21:53:12 +0600

    video 38 continue

--- src/com/company/java_08_ch_36_vi_01_main.java
+++ src/com/company/java_08_ch_36_vi_01_main.java
@@ -1,14 +1,42 @@
 package com.company;
+import java.util.Scanner;
 
-class name{
-    String name = "Shajidur Rahman";
+class details{
+    String name ;
+    int age;
+    String school;
+    public void prontDetails(){
+        System.out.println("My name is "+ name);
+        System.out.println("My age is " + age);
+        System.out.println("My School is " + school);
+    }
 }
 
 public class java_08_ch_36_vi_01_main {
     public static void main(String[] args) {
-        System.out.println("This is class");
-        name my_name = new name();
-        my_name.name = "My name is shajidur rahman";
-        System.out.println(my_name.name);
+        Scanner input = new Scanner(System.in);
+        System.out.println("------------------- Welcome to the details family -------------------");
+        details shajidur = new details();
+        shajidur.name = "Shajidur";
+        shajidur.age = 12;
+        shajidur.school = "Dinajpur Zilla School";
+
+        details mushfiq = new details();
+        mushfiq.name = "Mushfiq";
+        mushfiq.age = 12;
+        mushfiq.school = "Girls School";
+
+        System.out.print("Person name:  ");
+        String ask = input.nextLine();
+        if (ask == "shajidur"){
+            shajidur.prontDetails();
+        }
+        else if (ask == "mushfiq"){
+            mushfiq.prontDetails();
+        }
+        else {
+            System.out.println("Not found");
+        }
+
     }
 }
