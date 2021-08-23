535f63fa32d7544a5b7a2db04a37a12a6598ee3b
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-08-23 22:34:23 +0600

    video continue 53

--- src/com/company/java_11_ch_53_vi_01_main.java
+++ src/com/company/java_11_ch_53_vi_01_main.java
@@ -1,7 +1,25 @@
 package com.company;
+import java.util.Scanner;
+
+abstract class base2{
+    base2(){
+        System.out.println("I am the constructor of base 2");
+    }
+    public void sayHello(){
+        System.out.println("Hello !!");
+    }
+    abstract public void greet();
+}
+
+class child extends base2{
+    public void greet(){
+        System.out.println("Good morning");
+    }
+}
 
 public class java_11_ch_53_vi_01_main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
     }
 }
