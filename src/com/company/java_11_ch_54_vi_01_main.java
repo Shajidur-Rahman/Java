7414d8b2e8d662ddd222c3d88a2db62f616b6728
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-08-26 12:51:49 +0600

    video 55 finished

--- src/com/company/java_11_ch_54_vi_01_main.java
+++ src/com/company/java_11_ch_54_vi_01_main.java
@@ -1,7 +1,24 @@
 package com.company;
 
+interface Bicycle{
+    int a = 223;
+    void applyBreak(int decrement);
+    void speedUp(int increment);
+}
+
+class ayron implements Bicycle{
+    public void applyBreak(int decrement){
+        System.out.println("Applying break");
+    }
+    public void speedUp(int increment){
+        System.out.println("applying speed up");
+    }
+}
+
 public class java_11_ch_54_vi_01_main {
     public static void main(String[] args) {
-
+        ayron cycle = new ayron();
+        cycle.applyBreak(2);
+        System.out.println(cycle.a);
     }
 }
