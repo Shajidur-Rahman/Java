c887f02152975356be2a2100f09161241c3a07ac
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-08-28 22:08:44 +0600

    70 video continue

---
+++ notes/Chapter-12/exercise_05.java
@@ -0,0 +1,26 @@
+package com.shajidur.shape;
+
+class Rectangle{
+    Rectangle(int height, int width){
+        public int getHeight(){
+            return this.height;
+        }
+        public void setHeight(){
+            this.height = height;
+        }
+        
+        public int getWidth(){
+            return this.height;
+        }
+        public void setWidth(){
+            this.height = height;
+        }
+
+    }
+}
+
+public class exercise_05 {
+    public static void main(String[] args){
+        
+    }
+}
---
+++ src/com/company/java_13_ch_70_vi_01_main.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class java_13_ch_70_vi_01_main {
+    public static void main(String[] args) {
+
+    }
+}
