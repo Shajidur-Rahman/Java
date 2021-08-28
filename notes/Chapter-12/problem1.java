f3844000e65067741bed6f9f73f455212ad055f9
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-08-28 12:30:31 +0600

    chapter 12 finished

---
+++ notes/Chapter-12/problem1.java
@@ -0,0 +1,24 @@
+package calc;
+
+class Calculator{
+    public void claculate(int a, int b){
+        System.out.print("Your result is " + a + b);
+    }
+}
+class SeiCalculator{
+    public void claculate(int a, int b){
+        System.out.print("Your result is " + a + b);
+    }
+}
+class HyCalculator{
+    public void claculate(int a, int b){
+        System.out.print("Your result is " + a + b);
+    }
+}
+
+
+public class problem1 {
+    public static void main(String[] args) {
+        System.out.print("Code completed");
+    }
+}
--- src/com/company/java_12_ch_66_vi_01_main.java
+++ src/com/company/java_12_ch_66_vi_01_main.java
@@ -1,2 +1,21 @@
-package com.company;public class java_12_ch_66_vi_01_main {
+package com.company;
+
+class C1{
+    public int a = 12;
+    protected int b = 13;
+    int c = 14;
+    private int d = 15;
+    public void maht1(){
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
+
+public class java_12_ch_66_vi_01_main {
+    public static void main(String[] args) {
+        C1 c = new C1();
+        c.maht1();
+    }
 }
